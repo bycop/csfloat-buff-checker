1bc3652b3eb42d277d0d61552695f629b6d72727
Bycop <thebycop@gmail.com>
2023-08-11 23:32:20 +0200

    feat: Add Extension option page

--- no-api-version/background.js
+++ no-api-version/background.js
@@ -14,6 +14,21 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 chrome.action.onClicked.addListener(function (tab) {
-  chrome.tabs.create({'url': "https://github.com/bycop"}, function (tab) {
-  });
+	chrome.tabs.create({ 'url': "https://github.com/bycop" }, function (tab) {
+	});
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.action === 'getSettings') {
+		chrome.storage.local.get(['enableStickers', 'profitColor', 'lossColor', 'neutralColor'], function (result) {
+			const settings = {
+				enableStickers: result.enableStickers || true,
+				profitColor: result.profitColor || '#00FF00',
+				lossColor: result.lossColor || '#FF0000',
+				neutralColor: result.neutralColor || '#FFA500'
+			};
+			sendResponse(settings);
+		});
+		return true;
+	}
 });
---
+++ no-api-version/options.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const enableStickersOption = document.getElementById('enableStickers');
+	const profitColorOption = document.getElementById('profitColor');
+	const lossColorOption = document.getElementById('lossColor');
+	const neutralColorOption = document.getElementById('neutralColor');
+	const resetButton = document.getElementById('reset');
+
+	chrome.storage.local.get(['enableStickers', 'profitColor', 'lossColor', 'neutralColor'], function (result) {
+		enableStickersOption.checked = result.enableFeature || false;
+		profitColorOption.value = result.profitColor || '#00FF00';
+		lossColorOption.value = result.lossColor || '#FF0000';
+		neutralColorOption.value = result.neutralColor || '#FFA500';
+	});
+
+	enableStickersOption.addEventListener('change', function () {
+		const enableFeature = checkboxOptionOption.checked;
+		chrome.storage.local.set({ enableFeature });
+	});
+
+	profitColorOption.addEventListener('change', function () {
+		const profitColor = profitColorOption.value;
+		chrome.storage.local.set({ profitColor });
+	});
+
+	lossColorOption.addEventListener('change', function () {
+		const lossColor = lossColorOption.value;
+		chrome.storage.local.set({ lossColor });
+	});
+
+	neutralColorOption.addEventListener('change', function () {
+		const neutralColor = neutralColorOption.value;
+		chrome.storage.local.set({ neutralColor });
+	});
+
+	resetButton.addEventListener('click', function () {
+		chrome.storage.local.set({
+			enableStickers: false,
+			profitColor: '#00FF00',
+			lossColor: '#FF0000',
+			neutralColor: '#FFA500'
+		});
+		enableStickersOption.checked = false;
+		profitColorOption.value = '#00FF00';
+		lossColorOption.value = '#FF0000';
+		neutralColorOption.value = '#FFA500';
+	});
+});
